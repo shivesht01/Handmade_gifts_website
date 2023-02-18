f251e76737bddd1ed72298e1648115badd177514
Shivesh <shivesht01@gmail.com>
2023-02-18 23:06:02 +0530

    Deployment Related Changes

--- django_p1/urls.py
+++ django_p1/urls.py
@@ -11,3 +11,5 @@ urlpatterns = [
     path('contacts/', include('contacts.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
